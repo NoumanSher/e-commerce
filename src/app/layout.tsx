218e311681a4eb6ea9759589c05984a84268e328
NoumanSher <75526797+NoumanSher@users.noreply.github.com>
2024-08-25 01:32:57 +0500

    nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Jost } from "next/font/google"; // Import Jost instead of Inter
 import "./globals.css";
+import Navbar from "@/components/Nav";
 
-const inter = Inter({ subsets: ["latin"] });
+const jost = Jost({ subsets: ["latin"] }); // Load the Jost font
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jost.className}> {/* Apply Jost font class */}
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
