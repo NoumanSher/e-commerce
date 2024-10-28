f1d350c7cadd531b87915c5324fa6e278a9ab9a1
NoumanSher <75526797+NoumanSher@users.noreply.github.com>
2024-10-28 23:09:57 +0500

    change

--- src/components/productDetail/ProductDetails.tsx
+++ src/components/productDetail/ProductDetails.tsx
@@ -87,23 +87,23 @@ router.push('/pages/cart?section=checkout')
       /> */}
 
       <div className="flex md:items-center justify-between md:justify-normal mt-4 gap-x-4 mb-5">
-        {/* <QuantitySelector
+        <QuantitySelector
           quantity={quantity}
           setQuantity={setQuantity}
           className="h-14"
-        /> */}
-        {/* <Button
+        />
+        <Button
           onClick={handleAddToCart}
           className="rounded-none shadow-none bg-opacity-95 bg-black border-0 h-14 w-[50%] uppercase py-3 transition-all duration-500 hover:bg-white group hover:border border-black"
         >
           <p className="text-[14px] font-semibold leading-[1.72] group-hover:text-black">
             add to cart
           </p>
-        </Button> */}
+        </Button>
       </div>
 
       <div className="flex gap-x-7 items-center mb-5">
-        {/* <WishlistButton
+        <WishlistButton
           productId={id}
           title={title}
           price={price}
@@ -111,13 +111,13 @@ router.push('/pages/cart?section=checkout')
           quantity={quantity}
           color={selectedColor}
           size={selectedSize}
-        /> */}
+        />
         {/* <SocialMediaShare /> */}
       </div>
 
-      {/* <ProductMetaInfo sku="n/t4" categories="dresses,women" tags="dresses,women" /> */}
+      <ProductMetaInfo sku="n/t4" categories="dresses,women" tags="dresses,women" />
 
-      {/* <Tabs /> */}
+      <Tabs />
 
       <div className="flex justify-center mt-5">
         <CheckOutBtn className="!w-[100%]" onClick={handleCheckout} />
