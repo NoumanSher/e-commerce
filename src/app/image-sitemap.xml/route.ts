4978ccdad60728cb75fe973e40eca1b5095e2516
NoumanSher <75526797+NoumanSher@users.noreply.github.com>
2025-05-29 02:24:32 +0500

    error resolve

--- src/app/image-sitemap.xml/route.ts
+++ src/app/image-sitemap.xml/route.ts
@@ -1,12 +1,22 @@
-// app/image-sitemap.xml/route.ts
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const baseUrl = "https://pakshipper.com";
+  const baseUrl = "http://localhost:3000";
+  // const baseUrl = "https://pakshipper.com";
   const response = await fetch(
     "https://e-commerce-backend-seven-xi.vercel.app/api/products/get-all-products"
   );
   const data = await response.json();
+
+  // Escape special XML characters in text content
+  const escapeXml = (text: string) => {
+    return text
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
+  };
 
   let xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
@@ -14,13 +24,13 @@ export async function GET() {
 
   for (const product of data.data) {
     xml += `<url>
-      <loc>${baseUrl}/pages/product-detail/${product._id}</loc>`;
+      <loc>${escapeXml(`${baseUrl}/pages/product-detail/${product._id}`)}</loc>`;
     for (const img of product.images) {
       xml += `
         <image:image>
-          <image:loc>${img.src}</image:loc>
-          <image:title>${product.productName}</image:title>
-          <image:caption>${img.alt}</image:caption>
+          <image:loc>${escapeXml(img.src)}</image:loc>
+          <image:title>${escapeXml(product.productName)}</image:title>
+          <image:caption>${escapeXml(img.alt || '')}</image:caption>
         </image:image>`;
     }
     xml += `</url>`;
@@ -34,4 +44,3 @@ export async function GET() {
     },
   });
 }
-// This code generates an XML sitemap specifically for images of products in an e-commerce application.
