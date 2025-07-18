0a1889feff1c0a8c55c766abb37faa44266a94ea
NoumanSher <75526797+NoumanSher@users.noreply.github.com>
2025-07-18 18:51:23 +0500

    change 2

--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -1,4 +1,9 @@
+import { Suspense } from "react";
 import Register from "@/components/Register";
 export default function RegisterPage() {
-  return <Register />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Register />
+    </Suspense>
+  );
 }
